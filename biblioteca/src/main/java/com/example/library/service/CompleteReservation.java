3a48c2214a8a3ef4f1aed0a357c85b63c32618a9
Mayra Cano <mayra.cano@wizeline.com>
2024-10-28 10:51:14 -0600

    Correcciones

--- biblioteca/src/main/java/com/example/library/service/CompleteReservation.java
+++ biblioteca/src/main/java/com/example/library/service/CompleteReservation.java
@@ -11,9 +11,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 
-@Controller
+@Component
 public class CompleteReservation {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(CompleteReservation.class);
--- biblioteca/src/main/java/com/example/library/service/ReverseReservation.java
+++ biblioteca/src/main/java/com/example/library/service/ReverseReservation.java
@@ -8,9 +8,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 
-@Controller
+@Component
 public class ReverseReservation {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ReverseReservation.class);
